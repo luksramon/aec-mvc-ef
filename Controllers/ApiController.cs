cea628b0dc5d8e7f3e04421592eb12feffd67a69
luksramon <hp_lucas_@outlook.com>
2021-08-24 17:51:24 -0300

    Descomentar:
    public IActionResult Error()

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -23,10 +23,10 @@ namespace aec_mvc_entity_framework.Controllers
             return View();
         }
 
-        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        // public IActionResult Error()
-        // {
-        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        // }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
